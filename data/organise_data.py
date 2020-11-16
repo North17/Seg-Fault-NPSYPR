ba2ded595f7d37be575423c942d18db947e5ad06
Hariharan <jhariharan2004@gmail.com>
2020-11-16 14:31:06 +0530

    improved folder organisation

---
+++ data/organise_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+cases = pd.read_csv('../given_files/COVID_Dataset.csv')
+pop = pd.read_csv('../given_files/Population.csv')
+
+def write_ages():
+    pass
+
+def write_time_affected():
+    pass
+
+def write_zones():
+    pass
+    
