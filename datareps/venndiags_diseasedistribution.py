1696b8b4022d8747cd64a06770ec015d03d4c9f4
rayxv <74356650+rayxv@users.noreply.github.com>
2020-11-18 21:37:56 +0530

    Add files via upload

---
+++ datareps/venndiags_diseasedistribution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+venn3(subsets = (106, 37, 43, 44, 60, 11, 31), set_labels = ('Diabetes', 'Respiratory Ilnesses', 'Abnormal B.P'), alpha = 0.6);
+#truevaluesare/106698, 37058, 42913, 44078, 59862, 11393, 30631/
+
+
+# In[ ]:
+
+
+
+
