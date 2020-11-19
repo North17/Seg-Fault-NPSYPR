d2a20ac6a8322788edc602d767b3f5964d739866
North17 <69078717+North17@users.noreply.github.com>
2020-11-19 09:10:22 +0530

    Marc (#2): Merged Marc into master
    * added time_infected to oragnise_data.py and write_data.py
    
    * minor addition to graph codes
    
    * new file: example graphs
    
    * added getting time_infected using get_data.py
    
    * changed graph codes; started work on line plotter
    
    * made a start on line chart script
    
    * Copy Pasted basic line graph program from tests.ipynb into new file line_graphs.py
    
    * Continued code on line_graphs.py , largely copy pasted existing code from same file and adapted for further use
    
    * Finished and corrected code for making all line charts with age as x axis
    
    * deleted unnecessary code; optimisation
    
    * small issue
    
    * Experimented with heatcharts
    
    * Did heatmaps with seaborn <3
    
    * obtained pngs for heatmaps of cases, deaths, population
    
    * missed a change
    
    * started redoing linechart python file
    
    * used identical logic for time infected as y axis
    
    * small fixes
    
    Co-authored-by: Hariharan <jhariharan2004@gmail.com>
    Co-authored-by: programmer-er <60424117+programmer-er@users.noreply.github.com>

---
+++ line_graphs.py
@@ -0,0 +1,151 @@
+import pandas as pd 
+from matplotlib import pyplot as plt 
+from data import get_data
+
+ctrstr = "0-2-0-189-2040-050101"
+
+params = ctrstr.split('-')
+
+x = int(params[0])                      #x
+
+y = int(params[1])                      #y
+
+cond = int(params[2])                   #conditions
+
+zone = int(params[3])                   #zone
+
+age_start = int(params[4][:2])          #ages
+age_end = int(params[4][2:])
+
+age_start = int(params[5][:3])         #times
+time_end = int(params[5][3:])   
+
+
+x_axis = []
+y_axis = []
+num = []
+denom = []
+
+
+reqlist = get_data.get(x, zone )
+
+if x == 0:
+    x_axis = range(age_start, age_end)
+elif x == 1:
+    x_axis = range(time_start, time_end)
+elif x == 2:
+    for i in range(len(reqlist)):
+        x_axis.append(reqlist[i]['population'])
+
+sum =  0
+num = []
+denom = []
+
+# Ages as x axis
+
+if x == 0:
+    if y == 0:
+        for i in x_axis:
+            for j in range(time_start, time_end):
+                sum += reqlist[i]['time_infected'][j]['total'][0]
+            y_axis.append(sum)
+            sum = 0
+    elif y == 1:
+        for i in x_axis:
+            for j in range(time_start, time_end):
+                sum += reqlist[i]['time_infected'][j]['total'][1]
+            y_axis.append(sum)
+            sum = 0
+    elif y == 2:
+        for i in x_axis:
+            for j in range(time_start, time_end):
+                sum += reqlist[i]['time_infected'][j]['total'][1]
+            num.append(sum)
+            sum = 0
+        for i in x_axis:
+            for j in range(time_start, time_end):
+                sum += reqlist[i]['time_infected'][j]['total'][0]
+            denom.append(sum)
+            sum = 0
+        for i in range(len(x_axis)):
+            y_axis.append((num[i]/denom[i])*100)
+
+    elif y == 4:
+        for i in x_axis:
+            for j in range(time_start, time_end):
+                sum += (reqlist[i]['time_infected'][j]['total'][0] - reqlist[i]['time_infected'][j]['total'][1])
+            num.append(sum)
+            sum = 0
+        for i in x_axis:
+            for j in range(time_start, time_end):
+                sum += reqlist[i]['time_infected'][j]['total'][0]
+            denom.append(sum)
+            sum = 0
+        for i in range(len(x_axis)):
+            y_axis.append((num[i]/denom[i])*100)
+
+# Time Infected as X Axis
+
+if x ==1:
+    if y == 0:
+        for i in x_axis:
+            for j in range(age_start, age_end):
+                sum += reqlist[i]['ages'][j]['total'][0]
+            y_axis.append(sum)
+            sum = 0
+    elif y == 1:    
+        for i in x_axis:
+            for j in range(age_start, age_end):
+                sum += reqlist[i]['ages'][j]['total'][1]
+            y_axis.append(sum)
+            sum = 0
+    elif y == 2:
+        for i in x_axis:
+            for j in range(age_start, age_end):
+                sum += reqlist[i]['ages'][j]['total'][1]
+            num.append(sum)
+            sum = 0
+        for i in x_axis:
+            for j in range(age_start, age_end):
+                sum += reqlist[i]['ages'][j]['total'][0]
+            denom.append(sum)
+            sum = 0
+        for i in range(len(x_axis)):
+            y_axis.append((num[i]/denom[i])*100)
+
+    elif y == 4:
+        for i in x_axis:
+            for j in range(age_start, age_end):
+                sum += (reqlist[i]['ages'][j]['total'][0] - reqlist[i]['ages'][j]['total'][1])
+            num.append(sum)
+            sum = 0
+        for i in x_axis:
+            for j in range(age_start, age_end):
+                sum += reqlist[i]['ages'][j]['total'][0]
+            denom.append(sum)
+            sum = 0
+        for i in range(len(x_axis)):
+            y_axis.append((num[i]/denom[i])*100)
+
+# Population Density as X Axis
+
+if x == 2:
+    if y == 0:
+        
+    elif y == 1:
+
+    elif y == 2:
+
+    elif y == 3:
+
+    elif y == 4:
+
+
+
+
+
+
+
+
+
+
