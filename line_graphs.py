7885e19c8495adc6ba878db3b486600c06e1c8cc
Marcman <northstar1702@outlook.com>
2020-11-19 19:07:28 +0530

    finished most of line charts py file

--- line_graphs.py
+++ line_graphs.py
@@ -2,7 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt 
 from data import get_data
 
-ctrstr = "0-2-0-189-2040-050101"
+ctrstr = "1-1-2-189-2040-050101"
 
 params = ctrstr.split('-')
 
@@ -17,7 +17,7 @@ zone = int(params[3])                   #zone
 age_start = int(params[4][:2])          #ages
 age_end = int(params[4][2:])
 
-age_start = int(params[5][:3])         #times
+time_start = int(params[5][:3])         #times
 time_end = int(params[5][3:])   
 
 
@@ -37,115 +37,170 @@ elif x == 2:
     for i in range(len(reqlist)):
         x_axis.append(reqlist[i]['population'])
 
+if cond == 0:
+    cond = 'total'
+elif cond == 1:
+    cond = 'db'
+elif cond == 2:
+    cond = 'ri'
+elif cond == 3:
+    cond = 'bp'
+elif cond == 4:
+    cond = 'two'
+elif cond == 5:
+    cond = 'three'
+
 sum =  0
 num = []
 denom = []
 
-# Ages as x axis
+try:
+    # Ages as x axis
+    if x == 0:
+        if y == 0:
+            for i in x_axis:
+                for j in range(time_start, time_end):
+                    sum += reqlist[i]['time_infected'][j][cond][0]
+                y_axis.append(sum)
+                sum = 0
+        elif y == 1:
+            for i in x_axis:
+                for j in range(time_start, time_end):
+                    sum += reqlist[i]['time_infected'][j][cond][1]
+                y_axis.append(sum)
+                sum = 0
+        elif y == 2:
+            for i in x_axis:
+                for j in range(time_start, time_end):
+                    sum += reqlist[i]['time_infected'][j][cond][1]
+                num.append(sum)
+                sum = 0
+            for i in x_axis:
+                for j in range(time_start, time_end):
+                    sum += reqlist[i]['time_infected'][j][cond][0]
+                denom.append(sum)
+                sum = 0
+            for i in range(len(x_axis)):
+                y_axis.append((num[i]/denom[i])*100)
 
-if x == 0:
-    if y == 0:
-        for i in x_axis:
-            for j in range(time_start, time_end):
-                sum += reqlist[i]['time_infected'][j]['total'][0]
-            y_axis.append(sum)
-            sum = 0
-    elif y == 1:
-        for i in x_axis:
-            for j in range(time_start, time_end):
-                sum += reqlist[i]['time_infected'][j]['total'][1]
-            y_axis.append(sum)
-            sum = 0
-    elif y == 2:
-        for i in x_axis:
-            for j in range(time_start, time_end):
-                sum += reqlist[i]['time_infected'][j]['total'][1]
-            num.append(sum)
-            sum = 0
-        for i in x_axis:
-            for j in range(time_start, time_end):
-                sum += reqlist[i]['time_infected'][j]['total'][0]
-            denom.append(sum)
-            sum = 0
-        for i in range(len(x_axis)):
-            y_axis.append((num[i]/denom[i])*100)
+        elif y == 4:
+            for i in x_axis:
+                for j in range(time_start, time_end):
+                    sum += (reqlist[i]['time_infected'][j][cond][0] - reqlist[i]['time_infected'][j][cond][1])
+                num.append(sum)
+                sum = 0
+            for i in x_axis:
+                for j in range(time_start, time_end):
+                    sum += reqlist[i]['time_infected'][j][cond][0]
+                denom.append(sum)
+                sum = 0
+            for i in range(len(x_axis)):
+                y_axis.append((num[i]/denom[i])*100)
 
-    elif y == 4:
-        for i in x_axis:
-            for j in range(time_start, time_end):
-                sum += (reqlist[i]['time_infected'][j]['total'][0] - reqlist[i]['time_infected'][j]['total'][1])
-            num.append(sum)
-            sum = 0
-        for i in x_axis:
-            for j in range(time_start, time_end):
-                sum += reqlist[i]['time_infected'][j]['total'][0]
-            denom.append(sum)
-            sum = 0
-        for i in range(len(x_axis)):
-            y_axis.append((num[i]/denom[i])*100)
+    # Time Infected as X Axis
 
-# Time Infected as X Axis
+    if x ==1:
+        if y == 0:
+            for i in x_axis:
+                for j in range(age_start, age_end):
+                    sum += reqlist[i]['ages'][j][cond][0]
+                y_axis.append(sum)
+                sum = 0
+        elif y == 1:    
+            for i in x_axis:
+                for j in range(age_start, age_end):
+                    sum += reqlist[i]['ages'][j][cond][1]
+                y_axis.append(sum)
+                sum = 0
+        elif y == 2:
+            for i in x_axis:
+                for j in range(age_start, age_end):
+                    sum += reqlist[i]['ages'][j][cond][1]
+                num.append(sum)
+                sum = 0
+            for i in x_axis:
+                for j in range(age_start, age_end):
+                    sum += reqlist[i]['ages'][j][cond][0]
+                denom.append(sum)
+                sum = 0
+            for i in range(len(x_axis)):
+                y_axis.append((num[i]/denom[i])*100)
 
-if x ==1:
-    if y == 0:
-        for i in x_axis:
-            for j in range(age_start, age_end):
-                sum += reqlist[i]['ages'][j]['total'][0]
-            y_axis.append(sum)
-            sum = 0
-    elif y == 1:    
-        for i in x_axis:
-            for j in range(age_start, age_end):
-                sum += reqlist[i]['ages'][j]['total'][1]
-            y_axis.append(sum)
-            sum = 0
-    elif y == 2:
-        for i in x_axis:
-            for j in range(age_start, age_end):
-                sum += reqlist[i]['ages'][j]['total'][1]
-            num.append(sum)
-            sum = 0
-        for i in x_axis:
-            for j in range(age_start, age_end):
-                sum += reqlist[i]['ages'][j]['total'][0]
-            denom.append(sum)
-            sum = 0
-        for i in range(len(x_axis)):
-            y_axis.append((num[i]/denom[i])*100)
+        elif y == 4:
+            for i in x_axis:
+                for j in range(age_start, age_end):
+                    sum += (reqlist[i]['ages'][j][cond][0] - reqlist[i]['ages'][j][cond][1])
+                num.append(sum)
+                sum = 0
+            for i in x_axis:
+                for j in range(age_start, age_end):
+                    sum += reqlist[i]['ages'][j][cond][0]
+                denom.append(sum)
+                sum = 0
+            for i in range(len(x_axis)):
+                y_axis.append((num[i]/denom[i])*100)
 
-    elif y == 4:
-        for i in x_axis:
-            for j in range(age_start, age_end):
-                sum += (reqlist[i]['ages'][j]['total'][0] - reqlist[i]['ages'][j]['total'][1])
-            num.append(sum)
-            sum = 0
-        for i in x_axis:
-            for j in range(age_start, age_end):
-                sum += reqlist[i]['ages'][j]['total'][0]
-            denom.append(sum)
-            sum = 0
-        for i in range(len(x_axis)):
-            y_axis.append((num[i]/denom[i])*100)
+    # Population Density as X Axis
 
-# Population Density as X Axis
+    filter_by, filterl, filteru = 'ages', age_start, age_end
 
-if x == 2:
-    if y == 0:
-        
-    elif y == 1:
+    if time_start != 0 or time_end != 239:
+        filter_by, filterl, filteru = 'time_infected', time_start, time_end
 
-    elif y == 2:
+    pop_index = list(range(len(x_axis)))
+    if x == 2:
+        if y == 0:
+            for i in pop_index:
+                for j in range(filterl, filteru):
+                    sum += reqlist[i][filter_by][j][cond][0]
+                y_axis.append(sum)
+                sum = 0
+        elif y == 1:
+            for i in pop_index:
+                for j in range(filterl, filteru):
+                    sum += reqlist[i][filter_by][j][cond][1]
+                y_axis.append(sum)
+                sum = 0
+        elif y == 2:
+            for i in pop_index:
+                for j in range(filterl, filteru):
+                    sum += reqlist[i][filter_by][j][cond][0]
+                num.append(sum)
+                sum = 0
+            for i in pop_index:
+                denom.append(reqlist[i]['population'])
+            for i in pop_index:    
+                y_axis.append((num[i]/denom[i])*100)
+                
+        elif y == 3:
+            for i in pop_index:
+                for j in range(filterl, filteru):
+                    sum += reqlist[i][filter_by][j][cond][1]
+                num.append(sum)
+                sum = 0
+            for i in pop_index:
+                for j in range(filterl, filteru):
+                    sum += reqlist[i][filter_by][j][cond][0]
+                denom.append(sum)
+                sum = 0
+            for i in pop_index:
+                y_axis.append((num[i]/denom[i])*100)
+        elif y == 4:
+            for i in pop_index:
+                for j in range(filterl, filteru):
+                    sum += (reqlist[i][filter_by][j][cond][0] - reqlist[i][filter_by][j][cond][1])
+                num.append(sum)
+                sum = 0
+            for i in pop_index:
+                for j in range(filterl, filteru):
+                    sum += reqlist[i][filter_by][j][cond][0]
+                denom.append(sum)
+                sum = 0
+            for i in pop_index:
+                y_axis.append((num[i]/denom[i])*100)
+    plt.plot(x_axis, y_axis)
+    plt.show()
+except:
+    print("lol idk")
 
-    elif y == 3:
-
-    elif y == 4:
-
-
-
-
-
-
-
-
-
-
+    
