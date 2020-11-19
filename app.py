d0b7f4578b164914526d7890dcdba64a4c0738c5
programmer-er <60424117+programmer-er@users.noreply.github.com>
2020-11-20 00:48:52 +0530

    completed routes /heat and /infographics

--- app.py
+++ app.py
@@ -6,13 +6,36 @@ app = Flask(__name__)
 def index(): 
     return render_template('index.html')
 
+
+
+
 @app.route('/line')
 def line():
     return render_template('line.html')
 
+
+
+    
+@app.route('/heat')
+def heat():
+    return render_template('heat.html')
+
+
+    
+@app.route('/infographics')
+def infographics():
+    return render_template('infographics.html')
+
+
+
+
+
 @app.route('/custom')
 def custom():
     return render_template('custom.html')
+
+
+
 
 
 if __name__ == "__main__":
